d70bbc734a7b0f7827e0bc7af2405ce31dd061a3
ratopi <ratopi@abwesend.de>
2013-10-11 12:24:19 +0200

    retry if failed

--- src/00-update.sh
+++ src/00-update.sh
@@ -9,13 +9,19 @@ cd "$( dirname "$0" )"
 # load "master"
 curl -k -o directory.json. "$directoryURL"  &&  mv directory.json. directory.json
 
-# create local directory.json
-
 # load state jsons
 grep ':' directory.json | sed -e 's:" *,::' -e 's:"::g' -e 's+:+ +' | sort | while read id url; do
+
 	if curl -k -o "$id.json." "$url"; then
 
-		mv "$id.json." "$id.json"
+		for n in $( seq 1 10 ); do
+			if ! grep '<html' "$id.json."; then break; fi
+			echo "received HTML file ... trying again '$id' ($n)"
+			curl -k -o "$id.json." "$url"
+		done
+
+		! grep '<html' "$id.json."  &&  mv "$id.json." "$id.json"
 
 	fi
 done
+
